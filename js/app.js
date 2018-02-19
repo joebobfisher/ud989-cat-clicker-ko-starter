1aeabbbb239adb70e03c02e740a8b2e4479ee69a
Joe Fisher <fisher.joe@gmail.com>
2018-02-18 22:30:00 -0600

    Multi-cat!

--- js/app.js
+++ js/app.js
@@ -1,3 +1,31 @@
+var initialCats = [
+  { clickCount: 0,
+    name: 'Tabby',
+    imgSrc: 'img/434164568_fea0ad4013_z.jpg',
+    imgAttribution: 'https://www.flickr.com/photos/bigtallguy/434164568',
+    nicknames: ['Tabby', 'Tabbo', 'Tabitha Tabby Tabberoosky III, esq.', 'Merv'] },
+  { clickCount: 0,
+    name: 'Tiger',
+    imgSrc: 'img/4154543904_6e2428c421_z.jpg',
+    imgAttribution: 'https://www.flickr.com/photos/xshamx/4154543904',
+    nicknames: ['Tigger'] },
+  { clickCount: 0,
+    name: 'Scaredy',
+    imgSrc: 'img/22252709_010df3379e_z.jpg',
+    imgAttribution: 'https://www.flickr.com/photos/kpjas/22252709',
+    nicknames: ['Casper'] },
+  { clickCount: 0,
+    name: 'Shadow',
+    imgSrc: 'img/1413379559_412a540d29_z.jpg',
+    imgAttribution: 'https://www.flickr.com/photos/malfet/1413379559',
+    nicknames: ['Shooby'] },
+  { clickCount: 0,
+    name: 'Sleepy',
+    imgSrc: 'img/9648464288_2516b35537_z.jpg',
+    imgAttribution: 'https://www.flickr.com/photos/onesharp/9648464288',
+    nicknames: ['Zzzzz'] },
+];
+
 var Cat = function(data) {
   this.clickCount = ko.observable(data.clickCount);
   this.name = ko.observable(data.name);
@@ -32,17 +60,21 @@ var ViewModel = function() {
   // incrementCounter() because it is within a "with" binding.
   var self = this;
 
-  this.currentCat = ko.observable(new Cat({
-    clickCount: 0,
-    name: 'Tabby',
-    imgSrc: 'img/434164568_fea0ad4013_z.jpg',
-    imgAttribution: 'https://www.flickr.com/photos/big',
-    nicknames: ['Tabby', 'Tabbo', 'Tabitha Tabby Tabberoosky III, esq.', 'Merv']
-  }));
+  this.catList = ko.observableArray([]);
+
+  initialCats.forEach(function(catItem) {
+    self.catList.push(new Cat(catItem));
+  });
+
+  this.currentCat = ko.observable(this.catList()[0]);
 
   this.incrementCounter = function() {
         self.currentCat().clickCount(self.currentCat().clickCount() + 1);
-    };
+  };
+
+  this.setCurrentCat = function(clickedCat) {
+    self.currentCat(clickedCat);
+  };
 }
 
 ko.applyBindings(new ViewModel());
