4999db7d84762907c24278813435a3fc8ed7207e
Joe Fisher <fisher.joe@gmail.com>
2018-02-18 21:59:57 -0600

    Initialize Cat() from passed-in data.

--- js/app.js
+++ js/app.js
@@ -1,15 +1,9 @@
-var Cat = function() {
-  this.clickCount = ko.observable(0);
-  this.name = ko.observable('Tabby');
-  this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-  this.imgAttribution = ko.observable('https://www.flickr.com/photos/big');
-
-  this.nicknames = ko.observableArray(
-    [ { nickname: "Tabby" },
-      { nickname: "Tabbo" },
-      { nickname: "Tabitha Tabby Tabberoosky III, esq."},
-      { nickname: "Merv" } ]
-  );
+var Cat = function(data) {
+  this.clickCount = ko.observable(data.clickCount);
+  this.name = ko.observable(data.name);
+  this.imgSrc = ko.observable(data.imgSrc);
+  this.imgAttribution = ko.observable(data.imgAttribution);
+  this.nicknames = ko.observableArray(data.nicknames);
 
   this.level = ko.computed(function() {
     var level;
@@ -38,7 +32,13 @@ var ViewModel = function() {
   // incrementCounter() because it is within a "with" binding.
   var self = this;
 
-  this.currentCat = ko.observable(new Cat());
+  this.currentCat = ko.observable(new Cat({
+    clickCount: 0,
+    name: 'Tabby',
+    imgSrc: 'img/434164568_fea0ad4013_z.jpg',
+    imgAttribution: 'https://www.flickr.com/photos/big',
+    nicknames: ['Tabby', 'Tabbo', 'Tabitha Tabby Tabberoosky III, esq.', 'Merv']
+  }));
 
   this.incrementCounter = function() {
         self.currentCat().clickCount(self.currentCat().clickCount() + 1);
