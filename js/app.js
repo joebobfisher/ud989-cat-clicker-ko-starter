2a082335266dcb2cd8ccb0b8a982ebb5de1ccd58
Joe Fisher <fisher.joe@gmail.com>
2018-02-18 20:09:57 -0600

    Levels.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,28 @@
 var ViewModel = function() {
     this.clickCount = ko.observable(0);
     this.name = ko.observable('Tabby');
+    this.level = ko.observable('Infant');
     this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
     this.imgAttribution = ko.observable('https://www.flickr.com/photos/big');
 
     this.incrementCounter = function() {
         this.clickCount(this.clickCount() + 1);
+
+        if (this.clickCount() >= 50) {
+          this.level('Cattus Maximus');
+        }
+        else if (this.clickCount() >= 30) {
+          this.level('Cat');
+        }
+        else if (this.clickCount() >= 20) {
+          this.level('Teen');
+        }
+        else if (this.clickCount() >= 10) {
+          this.level('Kitty');
+        }
+        else {
+          this.level('Infant');
+        }
     };
 }
 
